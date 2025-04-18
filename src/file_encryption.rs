cde8e8aab6097e0680d6cd980cc56f3f7595a203
arthurfary <arthurfary22@gmail.com>
2025-04-18 20:06:48 -0300

    salt is now acctually random every time

---
+++ src/file_encryption.rs
@@ -0,0 +1,3 @@
+fn create_encrypted_file(filename: String, b64_salt: String, nonce: &[u8], b64_content: String) {}
+
+fn decrypt_file() {}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use argon2::Argon2;
-use argon2::password_hash::Error;
+use argon2::password_hash::rand_core::RngCore;
+use base64::prelude::*;
 use chacha20poly1305::aead::{Aead, AeadCore, KeyInit, OsRng};
-use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
+use chacha20poly1305::{ChaCha20Poly1305, Key};
 
 //TODO: Add better, general error handling
 //add notes on whats happening excalty
 //print encrypted as string
 
 fn main() {
+    // setting salt and pass manualy here for testing
     let pass = b"test";
-    let salt = b"randomsalt";
+    // let salt = b"randomsalt";
+    let message = b"test message";
+
+    // random salt
+    let mut random_salt = [0u8; 16];
+    OsRng.fill_bytes(&mut random_salt);
+
+    print!("random salt {:?}", random_salt);
+
     let mut output_key = [0u8; 32];
 
-    if let Err(e) = Argon2::default().hash_password_into(pass, salt, &mut output_key) {
+    if let Err(e) = Argon2::default().hash_password_into(pass, &random_salt, &mut output_key) {
         eprintln!("Argon2 error: {:?}", e);
         return;
     }
@@ -20,7 +30,6 @@ fn main() {
 
     let cypher = ChaCha20Poly1305::new(Key::from_slice(&output_key));
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
-    let message = b"test message";
 
     let cyphertext = match cypher.encrypt(&nonce, message.as_ref()) {
         Ok(text) => text,
@@ -31,12 +40,20 @@ fn main() {
     };
     println!("Cyphertext: {:?}", cyphertext);
 
+    // encode to bas64
+    let b64_text = BASE64_STANDARD.encode(&cyphertext);
+    let b64_salt = BASE64_STANDARD.encode(&random_salt);
+    println!(
+        "TEXT THAT WILL GO IN FILE:\nSALT: {}\nCONTENT:{}",
+        b64_salt, b64_text
+    );
+
     let mut key_bytes_decrypt = [0u8; 32];
-    if let Err(e) = Argon2::default().hash_password_into(pass, salt, &mut key_bytes_decrypt) {
+    if let Err(e) = Argon2::default().hash_password_into(pass, &random_salt, &mut key_bytes_decrypt)
+    {
         eprintln!("Argon2 error: {:?}", e);
         return;
     }
-
     let cipher_decrypt = ChaCha20Poly1305::new(Key::from_slice(&key_bytes_decrypt));
     let decrypted_bytes = match cipher_decrypt.decrypt(&nonce, cyphertext.as_ref()) {
         Ok(bytes) => bytes,
