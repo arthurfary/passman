e99c21a2f36d486252f61fe64b4c95f52e865fd1
Arthur Eduardo Fary <arthur@reveev.local>
2025-04-24 11:08:52 -0300

    cli: better handeling of params

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,14 @@ mod file_encryption;
 mod passman_encryption;
 use error::PassmanError;
 use rand::{Rng, RngCore, rng};
-use std::fs::read;
-use std::future::poll_fn;
 use std::io::{self, Write};
 use std::{env, fs};
 
-fn print_usage() {
-    println!("Password Manager CLI");
-    println!("Usage:");
-    println!("  new       - Create new password for a service");
-    println!("  register  - Register existing password for a service");
-    println!("  change    - Change password for a service");
-    println!("  list      - List all passwords files in password folder");
-    println!("  dlist     - Decrypt all");
-    println!("  help      - Show this help message\n");
-    println!("Examples:");
-    println!("  passman create");
-    println!("  passman register");
-    println!("  passman change");
-}
+//TODO:
+// - Find a better way of handeling the master password (Should be a parameter probably)
+// - Astract awaya all logic that doesn't need to be in this main, cli file
 
-fn read_input(prompt: &str) -> String {
+fn read_input(prompt: &str, is_pass: bool) -> String {
     print!("{}: ", prompt);
     io::stdout().flush().unwrap();
     let mut input = String::new();
@@ -31,47 +18,40 @@ fn read_input(prompt: &str) -> String {
     input.trim().to_string()
 }
 
-fn read_password(prompt: &str) -> String {
-    print!("{}: ", prompt);
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    input.trim().to_string()
+fn create_random_string(length: usize, charset: &[u8]) -> String {
+    let mut rng = rng();
+    (0..length)
+        .map(|_| {
+            let idx = rng.random_range(0..charset.len());
+            char::from(charset[idx])
+        })
+        .collect()
 }
 
 fn create_random_file_name(length: usize) -> String {
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let mut rng = rng();
-
-    (0..length)
-        .map(|_| {
-            let idx = rng.random_range(0..CHARSET.len());
-            char::from(CHARSET[idx])
-        })
-        .collect()
+    create_random_string(length, CHARSET)
 }
 
 fn create_random_password(length: usize) -> String {
     const CHARSET: &[u8] =
         b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
-    let mut rng = rng();
-
-    (0..length)
-        .map(|_| {
-            let idx = rng.random_range(0..CHARSET.len());
-            char::from(CHARSET[idx])
-        })
-        .collect()
+    create_random_string(length, CHARSET)
 }
 
-fn create_new_password() -> Result<(), PassmanError> {
-    let master_pwd = read_password("Master password:");
-    let service_name = read_input("Service name:");
+fn create_new_password(service_name: String) -> Result<(), PassmanError> {
+    let master_pwd = read_input("Master password", false);
     let random_pass = create_random_password(16);
+
+    let service_name = if service_name.is_empty() {
+        read_input("Enter service name", false)
+    } else {
+        service_name
+    };
 
     println!("{}", random_pass);
 
-    let gen_random_name = read_input("Generate random name? ([y]/n)").to_lowercase();
+    let gen_random_name = read_input("Generate random name? ([y]/n)", false).to_lowercase();
 
     let filename = if gen_random_name != "n" {
         create_random_file_name(8)
@@ -91,11 +71,16 @@ fn create_new_password() -> Result<(), PassmanError> {
     Ok(())
 }
 
-fn get_password() -> Result<(), PassmanError> {
-    let master_pwd = read_password("Enter master password");
-    let service_name = read_input("Enter service name");
+fn get_password(service_name: String) -> Result<(), PassmanError> {
+    let master_pwd = read_input("Enter master password", false);
 
-    let password_files = fs::read_dir(file_encryption::OUTPUT_PATH.to_owned()).unwrap();
+    let service_name = if service_name.is_empty() {
+        read_input("Enter service name", false)
+    } else {
+        service_name
+    };
+
+    let password_files = fs::read_dir(file_encryption::OUTPUT_PATH).unwrap();
 
     for file in password_files {
         let (file_service_name, service_password) = file_encryption::read_encrypted_file(
@@ -115,16 +100,43 @@ fn get_password() -> Result<(), PassmanError> {
     Ok(())
 }
 
-fn register_password() -> Result<(), PassmanError> {
-    Ok(())
-}
+fn register_password(service_name: String, service_pwd: String) -> Result<(), PassmanError> {
+    let master_pwd = read_input("Master password", true);
 
-fn change_password() -> Result<(), PassmanError> {
+    let service_name = if service_name.is_empty() {
+        read_input("Enter service name", false)
+    } else {
+        service_name
+    };
+
+    let service_pwd = if service_pwd.is_empty() {
+        read_input("Enter password", true)
+    } else {
+        service_pwd
+    };
+
+    let gen_random_name = read_input("Generate random name? ([y]/n)", false).to_lowercase();
+
+    let filename = if gen_random_name != "n" {
+        create_random_file_name(8)
+    } else {
+        service_name.clone()
+    };
+
+    file_encryption::create_encrypted_file(
+        &filename,
+        &master_pwd,
+        &service_name,
+        service_pwd.as_bytes(),
+    )?;
+
+    println!("Password file {} created for {}", filename, service_name);
+
     Ok(())
 }
 
 fn list_files() -> Result<(), PassmanError> {
-    let password_files = fs::read_dir(file_encryption::OUTPUT_PATH.to_owned()).unwrap();
+    let password_files = fs::read_dir(file_encryption::OUTPUT_PATH).unwrap();
 
     for file in password_files {
         println!("{}", file.unwrap().path().display())
@@ -134,15 +146,12 @@ fn list_files() -> Result<(), PassmanError> {
 }
 
 fn list_service_names() -> Result<(), PassmanError> {
-    let master_pwd = read_password("Enter master password");
+    let master_pwd = read_input("Enter master password", true);
 
-    // Read all files in the directory
-    let password_files = fs::read_dir(file_encryption::OUTPUT_PATH.to_owned()).unwrap();
+    let password_files = fs::read_dir(file_encryption::OUTPUT_PATH).unwrap();
 
-    // Create a vector to store service names
     let mut service_names: Vec<(String, String)> = Vec::new();
 
-    // Process each file
     for file in password_files {
         let (file_service_name, service_password) = file_encryption::read_encrypted_file(
             //FIXME: remove unwrap spam
@@ -152,7 +161,6 @@ fn list_service_names() -> Result<(), PassmanError> {
         service_names.push((file_service_name, service_password));
     }
 
-    // Print the service names
     if service_names.is_empty() {
         println!("No services found.");
     } else {
@@ -165,9 +173,14 @@ fn list_service_names() -> Result<(), PassmanError> {
     Ok(())
 }
 
-fn decrypt_file_prompt() -> Result<(), PassmanError> {
-    let master_pwd = read_password("Master Password:");
-    let filename = read_input("File to decrypt:");
+fn decrypt_file_prompt(filename: String) -> Result<(), PassmanError> {
+    let master_pwd = read_input("Master Password:", true);
+
+    let filename = if filename.is_empty() {
+        read_input("Enter service name", false)
+    } else {
+        filename
+    };
 
     let (service_name, service_password) =
         file_encryption::read_encrypted_file(&filename, &master_pwd)?;
@@ -177,28 +190,41 @@ fn decrypt_file_prompt() -> Result<(), PassmanError> {
     Ok(())
 }
 
+fn print_usage() {
+    println!("Passman password manager");
+    println!("Usage:");
+    println!("  new       - Create new password for a service");
+    println!("  decrypt   - Decrypt a file manually");
+    println!("  get       - Get a password for a service");
+    println!("  register  - Register existing password for a service");
+    println!("  list      - List all passwords files in password folder");
+    println!("  dall      - Decrypt all");
+    println!("  help      - Show this help message\n");
+    println!("Examples:");
+    println!("  passman new");
+    println!("  passman register");
+}
+
 fn main() -> Result<(), PassmanError> {
-    let args: Vec<String> = env::args().collect();
+    let mut args = env::args().skip(1); // skip binary name
 
-    if args.len() < 2 {
-        print_usage();
-        return Ok(());
-    }
-
-    match args[1].as_str() {
-        "new" => create_new_password()?,
-        "get" => get_password()?,
-        "decrypt" => decrypt_file_prompt()?,
-        "register" => register_password()?,
-        "change" => change_password()?,
-        "dall" => list_service_names()?,
-        "list" => list_files()?,
-        "help" => {
+    match args.next().as_deref() {
+        Some("new") => create_new_password(args.next().unwrap_or_default())?,
+        Some("get") => get_password(args.next().unwrap_or_default())?,
+        Some("decrypt") => decrypt_file_prompt(args.next().unwrap_or_default())?,
+        Some("register") => register_password(
+            args.next().unwrap_or_default(),
+            args.next().unwrap_or_default(),
+        )?,
+        Some("dall") => list_service_names()?,
+        Some("list") => list_files()?,
+        Some("help") => print_usage(),
+        Some(cmd) => {
+            println!("Unknown command: {}", cmd);
             print_usage();
             return Ok(());
         }
-        _ => {
-            println!("Unknown command: {}", args[1]);
+        None => {
             print_usage();
             return Ok(());
         }
