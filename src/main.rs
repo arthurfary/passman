1c326176297395695c087bbf27617ca6683642fe
arthurfary <arthurfary22@gmail.com>
2025-05-05 23:25:53 -0300

    refactor: Command struct, better func args

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod passman_encryption;
 use error::PassmanError;
 use rand::{Rng, rng};
 use rpassword;
-use std::env::args;
 use std::ffi::OsString;
 use std::io::{self, Write};
 use std::{env, fs};
@@ -50,7 +49,7 @@ fn create_random_password(length: usize) -> String {
     create_random_string(length, CHARSET)
 }
 
-fn create_new_password(service_name: String) -> Result<(), PassmanError> {
+fn create_new_password(command: Command) -> Result<(), PassmanError> {
     let master_pwd = read_input("Master password", true);
     let confirm_pwd = read_input("Retype master password", true);
 
@@ -62,10 +61,9 @@ fn create_new_password(service_name: String) -> Result<(), PassmanError> {
 
     let random_pass = create_random_password(16);
 
-    let service_name = if service_name.is_empty() {
-        read_input("Enter service name", false)
-    } else {
-        service_name
+    let service_name = match command.service {
+        Some(s) => s,
+        None => read_input("Enter service name", false),
     };
 
     println!("{}", random_pass);
@@ -89,13 +87,12 @@ fn create_new_password(service_name: String) -> Result<(), PassmanError> {
 
     Ok(())
 }
-fn get_password(service_name: String) -> Result<(), PassmanError> {
+fn get_password(command: Command) -> Result<(), PassmanError> {
     let master_pwd = read_input("Enter master password", true);
 
-    let service_name = if service_name.is_empty() {
-        read_input("Enter service name", false)
-    } else {
-        service_name
+    let service_name = match command.service {
+        Some(s) => s,
+        None => read_input("Enter service name", false),
     };
 
     let password_files = fs::read_dir(file_encryption::get_output_path()).unwrap();
@@ -115,19 +112,17 @@ fn get_password(service_name: String) -> Result<(), PassmanError> {
     Ok(())
 }
 
-fn register_password(service_name: String, service_pwd: String) -> Result<(), PassmanError> {
+fn register_password(command: Command) -> Result<(), PassmanError> {
     let master_pwd = read_input("Master password", true);
 
-    let service_name = if service_name.is_empty() {
-        read_input("Enter service name", false)
-    } else {
-        service_name
+    let service_name = match command.service {
+        Some(s) => s,
+        None => read_input("Enter service name", false),
     };
 
-    let service_pwd = if service_pwd.is_empty() {
-        read_input("Enter password", true)
-    } else {
-        service_pwd
+    let service_pwd = match command.existing_password {
+        Some(s) => s,
+        None => read_input("Paste or Type existing password", true),
     };
 
     let gen_random_name = read_input("Generate random name? ([y]/n)", false).to_lowercase();
@@ -206,37 +201,73 @@ fn print_usage() {
     println!("Passman password manager");
     println!("Usage:");
     println!("  new       - Create new password for a service");
-    println!("  decrypt   - Decrypt a file manually");
+    // println!("  decrypt   - Decrypt a file manually");
     println!("  get       - Get a password for a service");
     println!("  register  - Register existing password for a service");
     println!("  list      - List all passwords files in password folder");
-    println!("  dall      - Decrypt all");
+    // println!("  dall      - Decrypt all");
     println!("  help      - Show this help message\n");
     println!("Examples:");
     println!("  passman new");
     println!("  passman register");
 }
 
-fn main() -> Result<(), PassmanError> {
-    let mut args = env::args().skip(1); // skip binary name
+struct Command {
+    name: String,
+    service: Option<String>,
+    existing_password: Option<String>,
+}
 
-    match args.next().as_deref() {
-        Some("new") => create_new_password(args.next().unwrap_or_default())?,
-        Some("get") => get_password(args.next().unwrap_or_default())?,
-        Some("decrypt") => decrypt_file_prompt(args.next().unwrap_or_default())?,
-        Some("register") => register_password(
-            args.next().unwrap_or_default(),
-            args.next().unwrap_or_default(),
-        )?,
-        Some("dall") => list_service_names()?,
-        Some("list") => list_files()?,
-        Some("help") => print_usage(),
-        Some(cmd) => {
-            println!("Unknown command: {}", cmd);
-            print_usage();
-            return Ok(());
+impl Command {
+    fn new(args: &[String]) -> Command {
+        let name = args[1].clone();
+        let service = if args.len() > 2 {
+            Some(args[2].clone())
+        } else {
+            None
+        };
+        let existing_password = if args.len() > 3 {
+            Some(args[3].clone())
+        } else {
+            None
+        };
+
+        Command {
+            name,
+            service,
+            existing_password,
         }
-        None => {
+    }
+}
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect(); // skip binary name
+
+    if args.len() == 1 {
+        // if no args
+        print_usage();
+        return;
+    }
+
+    let command = Command::new(&mut args);
+
+    match run_command(command) {
+        Ok(_) => (),
+        Err(_) => println!("erro"),
+    }
+}
+
+fn run_command(command: Command) -> Result<(), PassmanError> {
+    match command.name.as_str() {
+        "new" => create_new_password(command)?,
+        "get" => get_password(command)?,
+        //"decrypt" => decrypt_file_prompt(command)?,  // Will be replaced with file reading
+        "register" => register_password(command)?,
+        //"dall" => list_service_names(command?)    // Will be replaced with file reading,
+        "list" => list_files()?,
+        "help" => print_usage(),
+        _ => {
+            println!("Unknown command: {}", command.name.as_str());
             print_usage();
             return Ok(());
         }
